d3a0dfc9735d5004eb9c05399d7a995c18d71b50
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-04-23 16:49:54 +0900

    first

---
+++ BoardVer1/src/main/java/com/koreait/board/BoardVO.java
@@ -0,0 +1,26 @@
+package com.koreait.board;
+
+public class BoardVO {
+	private int iboard;
+	private String title;
+	private String ctnt;
+	
+	public int getIboard() {
+		return iboard;
+	}
+	public void setIboard(int iboard) {
+		this.iboard = iboard;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCtnt() {
+		return ctnt;
+	}
+	public void setCtnt(String ctnt) {
+		this.ctnt = ctnt;
+	}
+}
---
+++ BoardVer1/src/main/java/com/koreait/board/Database.java
@@ -0,0 +1,8 @@
+package com.koreait.board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+	public static List<BoardVO> list = new ArrayList();
+}
